0d4553e9dcc72866a29ebb53b4d9199b9637dd8e
shennan <shennan@shenzone.com>
2022-09-09 01:10:36 +0800

    Add some unit test.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,37 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
-
+	@Test
+	void test01() {
+		assertTrue(true);
+	}
+	@Test
+	void test02() {
+		assertTrue(true);
+	}
+	@Test
+	void test03() {
+		assertTrue(true);
+	}
+	@Test
+	void test04() {
+		assertTrue(true);
+	}
+	@Test
+	void test05() {
+		assertTrue(true);
+	}
+	@Test
+	void test06() {
+		assertTrue(true);
+	}
 }
